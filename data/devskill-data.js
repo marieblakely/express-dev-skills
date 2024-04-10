370488e373821a3b798df60170a9f54e139839cb
Marie Blakely <marieblakely@myyahoo.com>
2024-04-10 18:45:20 -0400

    create array of fake data

---
+++ data/devskill-data.js
@@ -0,0 +1,9 @@
+const devskills = [
+  {skill: 'UI/UX', profiency: false, _id: 125223},
+  {skill: 'CSS', profiency: true, _id: 127904},
+  {skill: 'Back-end', profiency: false, _id: 139608},
+]
+
+export { 
+	devskills
+}
