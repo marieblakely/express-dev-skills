0be7021dd9a14960275966d349c1d84a1f09ba61
Marie Blakely <marieblakely@myyahoo.com>
2024-04-10 20:18:47 -0400

    built in

--- models/devskill.js
+++ models/devskill.js
@@ -3,9 +3,14 @@ import mongoose from 'mongoose'
 // optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema
 
-const Schema = mongoose.Schema
-	
-const todoSchema = new Schema({
+
+const devskillSchema = new Schema({
   text: String,
+  done: Boolean
 })
 
+const Devskill = mongoose.model('Devskill', devskillSchema)
+
+export {
+  Devskill
+}
