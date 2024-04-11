bd534e9456081907c3ea624952978c6dc1abeab9
Marie Blakely <marieblakely@myyahoo.com>
2024-04-10 20:11:01 -0400

    define schema

---
+++ models/devskill.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+// optional shortcut to the mongoose.Schema class
+const Schema = mongoose.Schema
+
+const Schema = mongoose.Schema
+	
+const todoSchema = new Schema({
+  text: String,
+})
+
